761ff7cfe1a73067cd9e0b336d35f60205465baa
PXJ <>
2020-03-12 18:16:48 +0800

    分离业务

---
+++ A_section/A_section/AViewController/AViewController.m
@@ -0,0 +1,27 @@
+//
+//  AViewController.m
+//  MainProject
+//
+//  Created by casa on 2016/12/10.
+//  Copyright © 2016年 casa. All rights reserved.
+//
+
+#import "AViewController.h"
+#import "BViewController.h"
+
+@interface AViewController ()
+
+@end
+
+@implementation AViewController
+
+#pragma mark - life cycle
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    self.view.backgroundColor = [UIColor whiteColor];
+}
+
+
+
+@end
